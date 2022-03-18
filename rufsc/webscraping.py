b7b36a38d146c73f8d45ed56d047a558b8a3ffa4
Luís Henrique Allebrandt Schünemann <aluishenrique01@gmail.com>
2022-03-18 14:38:00 -0300

    refactor: rename foods to meals

--- rufsc/webscraping.py
+++ rufsc/webscraping.py
@@ -15,7 +15,7 @@ def get_menu():
     for col in table_lines[0].find_all("td"):
         headers.extend(col.text.splitlines(False))
 
-    foods = []
+    meals = []
     table_lines.pop(0)
     for line in table_lines:
         for i, col in enumerate(line.find_all("td")):
@@ -25,20 +25,20 @@ def get_menu():
                 day = dt.datetime.strptime(menu_day, "%d.%m.%Y")
                 if day.date() != dt.datetime.today().date():
                     break
-            foods.append(item)
+            meals.append(item)
 
     # Maybe we can pack them up into a dict? \/
     # menu = {k: v for k, v in zip(headers, food)}
     # pprint(menu)
-    print_menu(headers, foods)
-    return headers, foods
+    print_menu(headers, meals)
+    return headers, meals
 
 
-def print_menu(headers, foods):
-    for header, food in zip(headers, foods):
+def print_menu(headers, meals):
+    for header, meal in zip(headers, meals):
         print(header)
-        for f in food:
-            print("  ", f)
+        for m in meal:
+            print("  ", m)
 
 
 if __name__ == "__main__":
