022df439f496d2ef654d572845d2634447648122
Luís Henrique Allebrandt Schünemann <aluishenrique01@gmail.com>
2022-04-15 14:50:45 -0300

    feat: get today's menu from Joinville RU

--- rufsc/webscraping.py
+++ rufsc/webscraping.py
@@ -1,36 +1,75 @@
 import datetime as dt
 
 import requests
+import tabula as tb
 from bs4 import BeautifulSoup
 
 
-def get_menu() -> dict:
-    url = "https://ru.ufsc.br/ru/"
+def get_menu():
+    # Request and find PDF menus
+    url = "https://restaurante.joinville.ufsc.br/cardapio-da-semana/"
     page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-    table = soup.find("div", {"class": "content clearfix"}).find("tbody")
-    table_lines = table.find_all("tr")
+    page_content = page.content
+    page_soup = BeautifulSoup(page_content, "html.parser")
 
-    headers = []
-    for col in table_lines[0].find_all("td"):
-        headers.extend(col.text.splitlines(False))
+    p = page_soup.find("p").find_all("a")
+    pdfs = [link.get("href") for link in p if "Cardápio" in link.get("href")]
 
-    meals = []
-    table_lines.pop(0)
-    for line in table_lines:
-        for i, col in enumerate(line.find_all("td")):
-            item = col.text.splitlines(False)
-            if i == 0:
-                menu_day = item[0].strip("\xa0")
-                day = dt.datetime.strptime(menu_day, "%d.%m.%Y")
-                if day.date() != dt.datetime.today().date():
-                    break
-            meals.append(item)
+    # Pandas DataFrame creation from PDF
+    pandas_options = {
+        "names": list(range(1, 8)),  # ISO weekday numbers (1=Monday, 2=Tuesday, etc.)
+    }
+    pdf_data = (
+        tb.read_pdf(pdfs[-1], pages=1, pandas_options=pandas_options, lattice=True)[0]
+        .drop([0, 1])  # Remove NaN and Column names
+        .reset_index()
+    )
 
-    menu = {k: v for k, v in zip(headers, meals)}
-    print_menu(menu)
+    # Extract data from the PDF Table
+    today_menu = None
+    ranges = [slice(11 * i, 11 * (1 + i)) for i in range(3)]  # 0:11, 11:22, 22:33
+    for i in range(3):
+        pdf_today_date = pdf_data[dt.datetime.today().isoweekday()][11 * i]
+        pdf_today_date = dt.datetime.strptime(pdf_today_date, "%d/%m/%Y").date()
 
-    return menu
+        # Get today's menu
+        if pdf_today_date == dt.datetime.today().date():
+            today_menu = pdf_data[dt.datetime.today().isoweekday()][ranges[i]]
+            break
+
+    if today_menu is not None:
+        menu = {
+            "Data": [],
+            "Preparações Fixas": [],
+            "Carne": [],
+            "Complemento": [],
+            "Saladas": [],
+            "Molho para salada": [],
+            "Sobremesa": [],
+        }
+        for i, item in enumerate(today_menu):
+            menu[get_menu_header(i)].append(item.capitalize())
+        return menu
+    else:
+        # No menu for today. Maybe they didn't add it yet.
+        return None
+
+
+def get_menu_header(i):
+    if i == 0:
+        return "Data"
+    elif 1 <= i <= 3:
+        return "Preparações Fixas"
+    elif i == 4:
+        return "Carne"
+    elif 5 <= i <= 6:
+        return "Complemento"
+    elif i == 7:
+        return "Molho para salada"
+    elif 8 <= i <= 9:
+        return "Saladas"
+    elif i == 10:
+        return "Sobremesa"
 
 
 def print_menu(menu: dict):
